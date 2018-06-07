c9b0d17e0e4cef45634785758ff9d22fbe11d07d
fastily <fastily@users.noreply.github.com>
2018-06-07 00:30:06 -0700

    added setup for wgen, improved arg handling in sup, added reqts txt file

--- sup.py
+++ sup.py
@@ -1,30 +1,59 @@
 import argparse
 import os
 import re
+import getpass
 
 from datetime import date, datetime
 
 from wiki import Wiki
 from wgen import Wgen
 
+MTC_FILE=".mtc.px.txt"
 
 def listdir_fullpath(d):
     return [os.path.join(d, f) for f in os.listdir(d)]
 
 
 cli_parser = argparse.ArgumentParser(description="Simple Commons Uploader")
-cli_parser.add_argument('dirs', metavar='folders', type=str, nargs='+', help='folders with files to upload')
-cli_parser.add_argument('--user', type=str, default="FSock")
-cli_parser.add_argument('--pw', type=str, default="")
+cli_parser.add_argument('dirs', metavar='folders', type=str, nargs='*', help='folders with files to upload')
+cli_parser.add_argument('--user', type=str, help="username to use")
+cli_parser.add_argument('--pw', type=str, help="password to use")
+cli_parser.add_argument("-i",  action='store_true', help="force interactive login")
+cli_parser.add_argument("--wgen",  action='store_true', help="run wgen password manager")
 args = cli_parser.parse_args()
+
+if args.wgen:
+    Wgen.setup(out_file=MTC_FILE, allow_continue=False)
+    exit()
 
 
 wiki = Wiki("en.wikipedia.org")
-if args.user: ## FIXME
-    if args.pw:
-        wiki.login(args.user, args.pw)
-    else:
-        wiki.login(args.user, Wgen.px_for(args.user))
+if args.i:
+    print("Please login to continue.")
+    u = input("Username: ")
+    p = getpass.getpass()
+    wiki.login(u, p)
+elif args.user:
+    if not args.pw:
+        print("ERROR: You didn't specify a password, --pw")
+        exit()
+
+    wiki.login(args.user, args.pw)
+elif os.path.isfile(MTC_FILE):
+    pxd = Wgen.load_px(px_file=MTC_FILE)
+    if not pxd:
+        print("ERROR: please run with --wgen option or remove {} from your home directory".format(MTC_FILE))
+
+    k, v = pxd.popitem()
+    wiki.login(k, v)
+else:
+    cli_parser.print_help()
+    exit()
+
+
+if not args.dirs:
+    print("You didn't specify and directories to upload!  Goodbye.")
+    exit()
 
 pattern = re.compile("(?i).+\\.({})".format("|".join(wiki.acceptable_file_extensions())))
 
--- wgen.py
+++ wgen.py
@@ -1,21 +1,51 @@
 import base64
 import os
+import re
+import getpass
 
 class Wgen:
     @staticmethod
-    def px_for(user):
+    def load_px(px_file=".px.txt"):
         pxd = {}
 
-        pxf = os.path.join(os.path.expanduser("~"), ".px.txt")
+        pxf = os.path.join(os.path.expanduser("~"), px_file)
         if os.path.isfile(pxf):
             with open(pxf, "r") as f:
                 for line in base64.b64decode(f.read().encode("utf-8")).decode("utf-8").strip().splitlines():
                     u, p = line.split("\t")
                     pxd[u] = p
                     
-        return pxd.get(user, None)
+        return pxd
 
 
-if __name__ == "__main__":
-    # base64.b64encode("nyah".encode("utf-8")).decode("utf-8")
-    pass
+    @staticmethod
+    def setup(out_file=".px.txt", allow_continue=True):
+
+        pxl = {}
+
+        while True:
+            print("Please enter the username/password combo(s) you would like to use.")
+            u = input("Username: ")
+            p = getpass.getpass()
+            confirm_p = getpass.getpass("Confirm Password: ")
+
+            if p != confirm_p:
+                print("ERROR: Entered passwords do not match")
+                if not re.match("(?i)(y|yes)", input("Try again? (y/N): ").lower()):
+                    break
+            else:
+                pxl[u] = p
+
+                if not allow_continue or not re.match("(?i)(y|yes)", input("Continue? (y/N): ").lower()):
+                    break
+
+        if not pxl:
+            print("WARNING: You did not make any entries.  Doing nothing.")
+            return
+
+        out = ""
+        for k,v in pxl:
+            out += "{}\t{}\n".format(k,v)
+
+        with open(os.path.join(os.path.expanduser("~"), out_file), "w") as f:
+            f.write(base64.b64encode(out.encode("utf-8")).decode("utf-8"))
