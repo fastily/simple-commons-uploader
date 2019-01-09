6d70dd0f494390baaf0ca4da376786a46266a677
fastily <fastily@users.noreply.github.com>
2019-01-08 18:15:20 -0800

    fix spacing, format

--- simple_commons_uploader/scu.py
+++ simple_commons_uploader/scu.py
@@ -5,22 +5,22 @@ import getpass
 
 from datetime import date, datetime
 
-from .wiki import Wiki, ColorLog
-from .wgen import Wgen
+from wiki import Wiki, ColorLog
+from wgen import Wgen
+
 
 def main():
-    MTC_FILE=os.path.join(os.path.expanduser("~"), ".scu.px.txt")
+    MTC_FILE = os.path.join(os.path.expanduser("~"), ".scu.px.txt")
 
     def listdir_fullpath(d):
         return [os.path.join(d, f) for f in os.listdir(d)]
-
 
     cli_parser = argparse.ArgumentParser(description="Simple Commons Uploader")
     cli_parser.add_argument('dirs', metavar='folders', type=str, nargs='*', help='folders with files to upload')
     cli_parser.add_argument('--user', type=str, help="username to use")
     cli_parser.add_argument('--pw', type=str, help="password to use")
-    cli_parser.add_argument("-i",  action='store_true', help="force interactive login")
-    cli_parser.add_argument("--wgen",  action='store_true', help="run wgen password manager")
+    cli_parser.add_argument("-i", action='store_true', help="force interactive login")
+    cli_parser.add_argument("--wgen", action='store_true', help="run wgen password manager")
     args = cli_parser.parse_args()
 
     if args.wgen:
@@ -50,27 +50,16 @@ def main():
         cli_parser.print_help()
         exit()
 
-
     if not args.dirs:
-        ColorLog.warn("You didn't specify and directories to upload!  Goodbye.")
+        ColorLog.warn(
+            "You didn't specify and directories to upload!  Goodbye.")
         exit()
 
-    pattern = re.compile("(?i).+\\.({})".format("|".join(wiki.acceptable_file_extensions())))
+    pattern = re.compile(
+        "(?i).+\\.({})".format("|".join(wiki.acceptable_file_extensions())))
 
-    tpl = """=={{{{int:filedesc}}}}==
-    {{{{Information
-    |description={}
-    |date={}
-    |source={{{{Own}}}}
-    |author=~~~
-    }}}}
-
-    =={{{{int:license-header}}}}==
-    {{{{Self|Cc-by-sa-4.0}}}}
-
-    [[Category:{}]]
-    [[Category:Files by {}]]"""
-
+    tpl = "=={{{{int:filedesc}}}}==\n{{{{Information\n|description={}\n|date={}\n|source={{{{Own}}}}\n|author=~~~\n}}}}\n\n" + \
+        "=={{{{int:license-header}}}}==\n{{{{Self|Cc-by-sa-4.0}}}}\n\n[[Category:{}]]\n[[Category:Files by {}]]"
 
     for d in args.dirs:
         if not os.path.isdir(d):
@@ -80,20 +69,22 @@ def main():
         dir_base = os.path.basename(os.path.normpath(d))
 
         fails = []
-        for f in [ fn for fn in listdir_fullpath(d) if pattern.match(fn) ]:
-            title = "{} {} {}{}".format(dir_base, i, str(date.today()), os.path.splitext(f)[1].lower())
-            desc = tpl.format(dir_base, f"{datetime.fromtimestamp(os.path.getmtime(f)):%Y-%m-%d %H:%M:%S}", dir_base, wiki.username)
+        for f in [fn for fn in listdir_fullpath(d) if pattern.match(fn)]:
+            title = "{} {} {}{}".format(dir_base, i, str(
+                date.today()), os.path.splitext(f)[1].lower())
+            desc = tpl.format(
+                dir_base, f"{datetime.fromtimestamp(os.path.getmtime(f)):%Y-%m-%d %H:%M:%S}", dir_base, wiki.username)
             if not wiki.upload(f, title, desc, ""):
                 fails.append(f)
-            i+=1
+            i += 1
 
     if fails:
         print("Failed to upload:")
         for f in fails:
             print(f)
     else:
-        print("All done!")
-
+        print("Complete, with no failures")
 
 if __name__ == '__main__':
     main()
+    
