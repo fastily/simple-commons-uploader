92d7c96bae3a9171327ffe89c314a30fee0cf225
fastily <fastily@users.noreply.github.com>
2020-07-22 23:55:26 -0700

    prep for next dev cycle

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="simple-commons-uploader",
-    version="0.2.0",
+    version="0.3.0",
     author="Fastily",
     author_email="fastily@users.noreply.github.com",
     description="Batch Wikimedia Commons Uploader",
