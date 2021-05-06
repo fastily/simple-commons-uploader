479a3f31ea6d9bb363e0f63912d2c08a00d153b7
fastily <fastily@users.noreply.github.com>
2021-05-06 02:58:51 -0700

    bugfix exif, save cookies when exiting

--- scu/__main__.py
+++ scu/__main__.py
@@ -6,8 +6,9 @@ import re
 from datetime import date, datetime
 from pathlib import Path
 from textwrap import dedent
+from typing import Any
 
-from PIL import ExifTags, Image
+from PIL import Image
 from rich.logging import RichHandler
 
 from pwiki import wgen
@@ -18,10 +19,21 @@ log = logging.getLogger(__name__)
 _MTC_FILE = Path.home() / ".scu.px.txt"
 
 
-def _main():
+def _make_wiki(*args: Any) -> Wiki:
+    """Convienence method, creates a new `Wiki` pointed at Commons with `args`
+
+    Returns:
+        Wiki: The Wiki resulting object
+    """
+    return Wiki("commons.wikimedia.org", *args)
+
+
+def _main() -> None:
+    """Main driver, to be run if this script is invoked directly."""
+
     for lg in (logging.getLogger("pwiki"), log):
         lg.addHandler(RichHandler(rich_tracebacks=True))
-        lg.setLevel("INFO")
+        lg.setLevel(logging.INFO)
 
     cli_parser = argparse.ArgumentParser(description="Simple Commons Uploader")
     cli_parser.add_argument('--user', type=str, help="username to use")
@@ -35,16 +47,15 @@ def _main():
         wgen.setup_px(_MTC_FILE, False)
         return
 
-    wiki = Wiki("commons.wikimedia.org")
     if args.i:
-        wiki.login(input("Please login to continue.\nUsername: "), getpass.getpass())
+        wiki = _make_wiki(input("Please login to continue.\nUsername: "), getpass.getpass())
     elif args.user:
         if not args.pw:
             log.critical("No password specified, please pass the --pw flag with a pasword.")
             return
-        wiki.login(args.user, args.pw)
+        wiki = _make_wiki(args.user, args.pw)
     elif _MTC_FILE.is_file():
-        wiki.login(*wgen.load_px(_MTC_FILE).popitem())
+        wiki = _make_wiki(*wgen.load_px(_MTC_FILE).popitem())
     else:
         cli_parser.print_help()
         return
@@ -62,15 +73,14 @@ def _main():
 
         i = 1
         for f in base_dir.iterdir():
-            ext = f.suffix.lower()
-            if not f.is_file() or ext not in ext_list:
+            if not f.is_file() or (ext := f.suffix.lower()) not in ext_list:
                 continue
 
             timestamp = None
             if ext in (".jpg", ".jpeg"):
                 try:
                     with Image.open(f) as img:
-                        if dto := next((v for k, v in img.getexif().items() if ExifTags.TAGS.get(k) == "DateTimeOriginal" and re.match(r"\d{4}:\d{2}:\d{2} \d{2}:\d{2}:\d{2}", v)), None):
+                        if (dto := img.getexif().get(306)) and re.match(r"\d{4}:\d{2}:\d{2} \d{2}:\d{2}:\d{2}", dto):
                             d, t = dto.split()
                             timestamp = f"{d.replace(':', '-')} {t}"
                 except Exception as e:
@@ -99,6 +109,8 @@ def _main():
         log.warning("Failed to upload %d files: %s", len(fails), fails)
     else:
         log.info("Finished with no failures")
+
+    wiki.save_cookies()
 
 
 if __name__ == '__main__':
