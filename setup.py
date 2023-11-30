7f2ef4820a6fc234f51bb596a7f546d00f325274
fastily <fastily@users.noreply.github.com>
2023-11-30 01:36:26 -0800

    tag for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="simple-commons-uploader",
-    version="0.3.3",
+    version="0.4.0",
     author="Fastily",
     author_email="fastily@users.noreply.github.com",
     description="Batch Wikimedia Commons Uploader",
